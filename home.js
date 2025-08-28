08d5b53f5bfdd4199946469ff1eb2f784ebc7542
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-08-29 00:00:29 +0600

    git add

--- home.js
+++ home.js
@@ -10,15 +10,21 @@ document.querySelectorAll(".add_love").forEach((btn) => {
 
 //call
 const callButtons = document.getElementsByClassName("callBtn");
-const balances = document.getElementsByClassName("callBalance");
+const balanceElem = document.querySelector(".callBalance"); 
 
-for (let i = 0; i < callButtons.length; i++) {
-  callButtons[i].addEventListener("click", function() {
-    
-    let balanceElem = balances[i];
-    let balance = parseInt(balanceElem.innerText);
+for (let btn of callButtons) {
+  btn.addEventListener("click", function() {
+    if (balanceElem) {
+      let balance = parseInt(balanceElem.innerText);
+      balance = Math.max(0, balance - 20);
+      balanceElem.innerText = balance;
+    }
 
-    balance = Math.max(0, balance - 20);
-    balanceElem.innerText = balance;
+const card = btn.closest("div.w-[455px]"); 
+if (card) {
+  const name = card.querySelector(".card-name").innerText;
+  const number = card.querySelector(".card-number").innerText; 
+  alert(`${name}\n${number}`); 
+}
   });
 }
